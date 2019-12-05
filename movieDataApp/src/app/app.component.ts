117fbfe65c901b230c7efb5885b508988f72d218
Duy Vu <dtv15@imperial.co.uk>
2019-12-05 00:08:42 +0000

    Added side menu

--- movieDataApp/src/app/app.component.ts
+++ movieDataApp/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +15,9 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private router: Router,
+    private authService: AuthService
   ) {
     this.initializeApp();
   }
@@ -24,4 +28,11 @@ export class AppComponent {
       this.splashScreen.hide();
     });
   }
+
+  onLogOut() {
+    console.log("LOGOUT IS CLICKED!!!!");
+    this.authService.logout();
+    this.router.navigateByUrl('/auth');
+  }
+
 }
